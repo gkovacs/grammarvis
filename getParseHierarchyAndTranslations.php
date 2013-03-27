09a98b92552c855ce3a8e97faba97bcd90e6c478
Geza Kovacs <gkovacs@mit.edu>
2013-03-27 18:52:33 -0400

    fixed content-type

--- getParseHierarchyAndTranslations.php
+++ getParseHierarchyAndTranslations.php
@@ -1,6 +1,6 @@
 <?php
 
-header('content-type', 'text/javascript');
+header('Content-type: text/javascript');
 $reqpath = $_SERVER["REQUEST_URI"];
 $reqpath = str_replace('getParseHierarchyAndTranslations.php', 'getParseHierarchyAndTranslations', $reqpath);
 echo file_get_contents('http://geza.csail.mit.edu:1357' . $reqpath);
